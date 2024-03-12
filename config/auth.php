9921b315c08eafd2029dcb6f9fb7a8843aae8ce7
diogo-antunes-h <diogo.antunes.h@gmail.com>
2024-03-11 23:13:22 -0300

    chore: update application configs and migrations

--- config/auth.php
+++ config/auth.php
@@ -62,7 +62,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => \App\Infrastructure\Database\Eloquent\Models\Client\Client::class,
         ],
 
         // 'users' => [
