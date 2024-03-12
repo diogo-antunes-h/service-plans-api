4ae2a6024b432f6180e5bc78c054c95f1410c13b
diogo-antunes-h <diogo.antunes.h@gmail.com>
2024-03-11 23:09:25 -0300

    feat: add more configs

--- app/Infrastructure/Shared/Http/Middleware/EncryptCookies.php
+++ app/Infrastructure/Shared/Http/Middleware/EncryptCookies.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace App\Infrastructure\Shared\Http\Middleware;
 
 use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
 
--- app/Infrastructure/Shared/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Infrastructure/Shared/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace App\Infrastructure\Shared\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
+use App\Application\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
